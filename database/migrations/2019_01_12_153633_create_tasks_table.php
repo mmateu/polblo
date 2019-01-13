4521130b641553c3c4e689f5481c57de44256696
mmateu <45851518+mmateu@users.noreply.github.com>
2019-01-13 01:04:41 +0100

    Create basic models, controllers and migrations

---
+++ database/migrations/2019_01_12_153633_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('start_time');
+            $table->enum('priority', ['low', 'medium', 'high', 'default']);
+            $table->unsignedInteger('card_id');
+            $table->foreign('card_id')->references('id')->on('cards'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
